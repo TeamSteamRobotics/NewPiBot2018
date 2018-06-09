55b068d390bdbf37d4f994835f159c7b126c2777
NotZack <zackarynichol@gmail.com>
2018-06-09 14:36:57 -0500

    Added Autonomous
    Who knows if it works, I have nothing to test on

--- fullyFeaturedDrive.py
+++ fullyFeaturedDrive.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import pigpio, curses, time
+import pigpio, curses, time, random
 
 scr=curses.initscr()
 scr.nodelay(1)
@@ -42,22 +42,66 @@ def setPins(ai1State, ai2State, bi1State, bi2State):
 	pi.write(bi1, bi1State)
 	pi.write(bi2, bi2State)
 	
-while True:
+def getDriveMode():
 	keyPressed = scr.getch()
-	if (keyPressed == ord('w')):
-		goForward()
-	elif (keyPressed == ord('s')):	
-		goBackwards()
-	elif (keyPressed == ord('a')):
-		goLeft()
-	elif (keyPressed == ord('d')):
-		goRight()
-	elif (keyPressed == ord('q')):
-		break
+	
+	print('Autonomous(y/n)')
+	if(keyPressed == ord('y') or keyPressed == ord('n')):
+	      if(keyPressed == ord('y')):
+	          return true
+	      else:
+	          return false
+	elif(keyPressed != 0):
+	      print('Invalid Input')
+	      getDriveMode()
+	      
+def initDrive():
+	autonomous = getDriveMode()
+	
+	if(autonomous):
+		autonomousDrive()
 	else:
-		stopMoving()
-		
-	curses.flushinp()
-	time.sleep(.04)
+	      userDrive()
+	      
+def autonomousDrive(mode):
+	if (mode == 'scripted'):
+		movements = ['forward', 'backwards', 'left', 'right']
 
+		for x in movements:
+			currentMovement = movements[x]
+
+			if (currentMovement == 'forward'):
+				goForward()
+			elif (currentMovement == 'backward'):
+				goBackwards()
+			elif (currentMovement == 'left'):
+				goLeft()
+			elif (currentMovement == 'right'):
+				goRight()
+
+			time.sleep(1)
+			stopMoving()
+	elif (mode == 'dance')
+		
+def userDrive():
+	while True:
+		keyPressed = scr.getch()
+
+		if (keyPressed == ord('w')):
+			goForward()
+		elif (keyPressed == ord('s')):	
+			goBackwards()
+		elif (keyPressed == ord('a')):
+			goLeft()
+		elif (keyPressed == ord('d')):
+			goRight()
+		elif (keyPressed == ord('q')):
+			break
+		else:
+			stopMoving()
+
+		curses.flushinp()
+		time.sleep(.04)
+
+initDrive()
 curses.endwin()
